736d5c29f441eff291e9cc6139a4cc2120bd950d
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2025-02-01 11:39:06 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-        proxy: {
-            '/api': {
-                target: import.meta.env.VITE_API_URL || 'http://localhost:3001',
-                changeOrigin: true,
-                secure: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, __dirname, '')
+    return {
+        plugins: [react()],
+        server: {
+            port: 5173,
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL || 'http://localhost:3001',
+                    changeOrigin: true,
+                    secure: false,
+                },
             },
         },
-    },
+    }
 }) 
